05a9fbadc048fd221b233825be8472a550069348
Shiv-hdfclife <shivprasadc@hdfclife.com>
2025-09-04 11:54:04 +0530

    Chnages

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -8,7 +8,7 @@ import axios from 'axios';
 //   },
 // });
 const axiosInstance = axios.create({
-  baseURL: "http://10.64.156.46:8080",
+  baseURL: "http://10.64.156.31:8080",
   headers: {
     'Content-Type': 'application/json',
   },
